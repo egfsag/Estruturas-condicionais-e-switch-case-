43c36d34abb5faffc81b2035b253ddf65242b381
Pedro Gabriel Oliveira <phezim08@gmail.com>
2025-11-10 17:22:20 -0300

    Create calendário_mês.cpp

---
+++ calendário_mês.cpp
@@ -0,0 +1,44 @@
+     
+    cout << "Digite um numero de 1 ao 12: ";
+    cin >> numero;
+    
+    switch (numero){
+        case 1 :
+            cout << "janeiro";
+        break;   
+            
+        case 2 :
+            cout << "fevereiro";
+        break;   
+        
+        case 3 :
+            cout << "março";
+        break;   
+        
+        case 4 :
+            cout << "abril";
+        break;   
+        
+        case 5 :
+            cout << "maio";
+        break;   
+        
+        case 6 :
+            cout << "junho";
+        break;   
+        
+        case 7 :
+            cout << "julho";
+        break;   
+        
+        case 8 :
+            cout << "agosto";
+        break;   
+        
+        case 9 :
+            cout << "setembro";
+        break;   
+        
+        case 10 :
+            cout << "outubro";
+        break;   
